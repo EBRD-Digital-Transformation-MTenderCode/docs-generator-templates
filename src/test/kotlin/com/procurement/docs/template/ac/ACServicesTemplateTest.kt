5666689e290e7b805c065d67c686afab73e2390b
Maxim Sambulat <maxim.sambulat@gmail.com>
2018-11-24 12:00:58 +0200

    Added generation pdf document to test

--- src/test/kotlin/com/procurement/docs/template/ac/ACServicesTemplateTest.kt
+++ src/test/kotlin/com/procurement/docs/template/ac/ACServicesTemplateTest.kt
@@ -6,19 +6,31 @@ import com.procurement.docs.toObject
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.thymeleaf.context.Context
+import org.xhtmlrenderer.pdf.ITextRenderer
+import java.io.File
+import java.io.FileOutputStream
 
 class ACServicesTemplateTest : AbstractTemplateTest() {
     private val templateEngine = templateEngine()
 
     @Test
     fun test() {
+        //Test context
         val storedJson = RESOURCES.load("json/domain/model/context/services.json")
         val data = mapper.toObject<ServicesContext>(storedJson)
         Assertions.assertNotNull(data)
 
+        //Test template
         val context = Context().apply {
             this.setVariable("context", data)
         }
-        templateEngine.process("services/services", context)
+        val html: String = templateEngine.process("services/services", context)
+
+        //Generate PDF
+        val outputStream = FileOutputStream(File("services.pdf"))
+        val renderer = ITextRenderer()
+        renderer.setDocumentFromString(html)
+        renderer.layout()
+        renderer.createPDF(outputStream)
     }
 }
