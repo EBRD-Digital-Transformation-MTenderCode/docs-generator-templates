dd42afa9391e0ddcb3a942f55a8ee23d7d91306e
volkov <mr.rotberry@gmail.com>
2018-11-26 14:49:42 +0200

    add new font

--- src/test/kotlin/com/procurement/docs/template/ac/ACServicesTemplateTest.kt
+++ src/test/kotlin/com/procurement/docs/template/ac/ACServicesTemplateTest.kt
@@ -1,5 +1,6 @@
 package com.procurement.docs.template.ac
 
+import com.lowagie.text.pdf.BaseFont
 import com.procurement.docs.domain.model.context.ServicesContext
 import com.procurement.docs.template.AbstractTemplateTest
 import com.procurement.docs.toObject
@@ -29,6 +30,8 @@ class ACServicesTemplateTest : AbstractTemplateTest() {
         //Generate PDF
         val outputStream = FileOutputStream(File("services.pdf"))
         val renderer = ITextRenderer()
+        val fontResourceURL = javaClass.classLoader.getResource("fonts/times-new-roman.ttf")
+        renderer.fontResolver.addFont(fontResourceURL.path, BaseFont.IDENTITY_H, BaseFont.EMBEDDED)
         renderer.setDocumentFromString(html)
         renderer.layout()
         renderer.createPDF(outputStream)
